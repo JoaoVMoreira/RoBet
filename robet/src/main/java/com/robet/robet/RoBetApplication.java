e31643da23e0b1f9e66176cd0e59e2045d7fbc67
JoaoVMoreira <jvittormoreira@outlook.pt>
2024-11-09 01:26:13 -0300

    first commit

---
+++ robet/src/main/java/com/robet/robet/RoBetApplication.java
@@ -0,0 +1,13 @@
+package com.robet.robet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoBetApplication.class, args);
+	}
+
+}
